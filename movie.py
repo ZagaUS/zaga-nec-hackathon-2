62bdd9bbc776ac7e3778f7b55f2c917c2890ee40
RaviArun44 <raviarun2004@gmail.com>
2026-02-06 16:45:01 +0530

    completed

---
+++ movie.py
@@ -0,0 +1,22 @@
+import pandas as pd
+df=pd.read_csv("spam_data.csv")
+a=pd.DataFrame.all
+print(a)
+b=pd.merge
+print(b)
+c=pd.DataFrame.add("num_link")
+print(c)
+
+print(b)
+
+for ch in a:
+    print(sum(list(ch)))
+ 
+
+for num in df.text:
+    if df.text==num:
+        print("numeric")
+    else:
+        print("non numeric")
+print(num)
+
